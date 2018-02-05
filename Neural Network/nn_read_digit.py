0a33344b867919bd5765b21d608f647eba115e5d
brianhscho <brianhscho@gmail.com>
2018-02-04 23:47:49 -0500

    Update nn_read_digit.py

--- Neural Network/nn_read_digit.py
+++ Neural Network/nn_read_digit.py
@@ -12,8 +12,8 @@ os.chdir = 'C:\\Users\\donkey\\Desktop\\machine learning\\Neural Network'
 1. Import neural network model, with trained parameters
 neural network - train weights and bias - dump parameters in json file
 '''
-model = NN([784, 30, 10])
-train,val,test = digit_data.load_data_wrapper()
+model = NN([784, 30, 10]) #untrained
+train, val, test = digit_data.load_data_wrapper()
 model.sgd(train, epoch = 50, batch_size = 10, eta = 2)   
 model.save("neural_network_digit.json") #with 10 reps of training
 
@@ -22,23 +22,7 @@ trained = json_file.load("neural_network_digit.json")  #load back trained parame
 
 
 '''
-2. testing text from cropped iamges
-'''
-a = Read("C:\\Users\\donkey\\Desktop\\machine learning\\Neural Network\\num.jpg",0.9,[28,28])
-plt.imshow(a.image, cmap="binary")
-b = a.resize()
-
-def image_predict(i):
-    b = np.reshape(i,(784,1))
-    output = trained.feed_forward(b)["activation"][-1]
-    return(np.where(output == max(output))[0][0])
-
-image_predict(b[4])
-
-
-
-'''
-3. testing testing data from NMIST
+2. testing data from NMIST
 '''
          
 def test_model(model,i):
@@ -69,10 +53,10 @@ predict(trained, len(test))
 predict(trained, 100)
 predict(model, 100)
 
-'model' (not trained model randomized initial parameters) serves no predicting function
+Un-trained model (randomized initial parameters) serves no predicting function
 with prediction rate of 10%, which would be the rate I would get by randomly choosing between 0 and 9.
 
-The trained model ( which takes trained weights and parameters from saved json file) 
+Trained model (trained weights and parameters from saved json file) 
 predicts 97% of test cases correctly.
 '''
 
