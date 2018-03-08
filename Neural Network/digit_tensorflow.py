42c875c57eb01235ac951b2beba28bbaaf196caf
brianhscho <brianhscho@gmail.com>
2018-03-08 15:34:27 -0500

    Update digit_tensorflow.py

--- Neural Network/digit_tensorflow.py
+++ Neural Network/digit_tensorflow.py
@@ -8,12 +8,10 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
 
 #parameters
 input_size = 784
-neuron_1 = 500
-neuron_2 = 500
-neuron_3 = 500
+neuron_1 = 30
 output_size = 10
-total_epochs = 10
-batch_size = 100
+total_epochs = 50
+batch_size = 10
 
 #define placeholders
 x = tf.placeholder('float', [None, input_size])
@@ -23,34 +21,21 @@ y = tf.placeholder('float', [None, output_size])
 def feed_forward(data):
     layer_1 = {"weights":tf.Variable(tf.random_normal([input_size, neuron_1])),
                "bias":tf.Variable(tf.random_normal([1, neuron_1]))}
-
-    layer_2 = {"weights":tf.Variable(tf.random_normal([neuron_1, neuron_2])),
-               "bias":tf.Variable(tf.random_normal([1, neuron_2]))}
-
-    layer_3 = {"weights":tf.Variable(tf.random_normal([neuron_2, neuron_3])),
-               "bias":tf.Variable(tf.random_normal([1, neuron_3]))}
-
-    output_layer = {"weights":tf.Variable(tf.random_normal([neuron_3, output_size])),
+    output_layer = {"weights":tf.Variable(tf.random_normal([neuron_1, output_size])),
                     "bias":tf.Variable(tf.random_normal([1, output_size]))}
     
     #t(W * x) = t(x) * t(W)
-    layer_1_out = tf.matmul(data, layer_1["weights"]) + layer_1["bias"]
-    layer_1_out = tf.nn.relu(layer_1_out)
+    layer_1_out = tf.matmul(data, layer_1["weights"])
+    layer_1_out_relu = tf.nn.sigmoid(layer_1_out)
     
-    layer_2_out = tf.matmul(layer_1_out, layer_2["weights"]) + layer_2["bias"]
-    layer_2_out = tf.nn.relu(layer_2_out)
-    
-    layer_3_out = tf.matmul(layer_2_out, layer_3["weights"]) + layer_3["bias"]
-    layer_3_out = tf.nn.relu(layer_3_out)
-    
-    output_out = tf.matmul(layer_3_out, output_layer["weights"]) + output_layer["bias"]
+    output_out = tf.matmul(layer_1_out_relu, output_layer["weights"])
     
     return(output_out)
 
-def test_train(x):
+def test_train(x, rate):
     y_hat = feed_forward(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = y_hat))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    cost = tf.reduce_mean(tf.square(y_hat - y))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate = rate).minimize(cost)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch in range(total_epochs):
@@ -71,4 +56,4 @@ def test_train(x):
         prediction_rate = tf.reduce_mean(tf.cast(list_test, 'float'))
         print("prediction rate: ", sess.run(prediction_rate, feed_dict = {x: mnist.test.images, y:mnist.test.labels}))
 
-test_train(x)
+test_train(x, 0.8) #prediction rate of 91%. Could improve
