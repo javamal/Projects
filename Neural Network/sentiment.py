0a662509f7752de7e09c7490f2c5e920e1488e44
brianhscho <brianhscho@gmail.com>
2018-03-20 02:17:23 -0400

    Update sentiment.py

--- Neural Network/sentiment.py
+++ Neural Network/sentiment.py
@@ -8,6 +8,7 @@ import pandas as pd
 np.set_printoptions(threshold=np.nan)
 
 with open("sentiment_data.pickle", "rb") as save_file:
+    print("loading raw text and label")
     sentiment = pickle.load(save_file)
     save_file.close()
 
@@ -34,22 +35,18 @@ def bag_of_words(lower, data = sentiment):
     bag = []
     for sentences in x_raw:        
         bag_of_words_item = np.zeros(len(total_frame))
-        word_token = nltk.tokenize.word_tokenize(sentences.lower()) 
-        '''
-        use sparse matrix?
-        '''
+        word_token = nltk.tokenize.word_tokenize(sentences.lower())        
         for word in word_token:
             if word in total_frame:            
                 bag_of_words_item[total_frame.index(word)] = bag_of_words_item[total_frame.index(word)] + 1
         if(sum(bag_of_words_item)==0):
             print(sentences)
         bag.append(bag_of_words_item)
-    return({"bag_of_words":bag, "label":y_raw, "text":x_raw})
+    return({"bag_of_words":bag, "label":y_raw, "text":x_raw, "frame":total_frame})
     
 def train_test(lower, test_portion):    
     data = bag_of_words(lower)
-    #random_index = np.random.permutation(len(data["bag_of_words"]))
-    random_index = np.arange(0, len(data["bag_of_words"]))
+    random_index = np.random.permutation(len(data["bag_of_words"]))    
     data["bag_of_words"] = np.array(data["bag_of_words"])[random_index]
     data["label"] = np.array(data["label"])[random_index]    
     index = int(len(data["bag_of_words"]) * test_portion)
@@ -66,3 +63,15 @@ def save_sentiment_bow(lower, test_portion, data = sentiment, file = "bagofwords
         save_file.close()
         print("saved as: file")
     return(True)
+    
+#save_sentiment_bow(lower = 75, test_portion = 0.1)    
+    
+    
+a = bag_of_words(50)
+
+i = 20200
+a["text"][i]    
+np.array(a["frame"])[np.where(a["bag_of_words"][i]==1)[0]]
+a["label"]     
+    
+    
