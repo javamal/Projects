7b0a9c1821c483a2bfb9f5f664e24f5a50f485bc
brianhscho <brianhscho@gmail.com>
2018-03-22 01:11:23 -0400

    Update and rename sentiment_data.py to Sentiment.py

--- Neural Network/Sentiment.py
+++ Neural Network/Sentiment.py
@@ -21,9 +21,9 @@ def review_sentiment(review_path):
             except:
                 print("error in: " + i)
             if label_raw == "1":
-                y.append([1,0,0])
+                y.append([1,0])
             elif label_raw == "0":
-                y.append([0,0,1])                
+                y.append([0,1])                
             text_raw = re.sub("\t([0|1])\n", "", i)
             x.append(text_raw)
         return({"x":x, "y":y})
@@ -39,41 +39,27 @@ def twitter_sentiment(twitter_path):
             except:
                 print("error in: " + i)
             if label_raw == "1":
-                y.append([1,0,0])
+                y.append([1,0])
             elif label_raw == "0":
-                y.append([0,0,1])                
+                y.append([0,1])                
             text_raw = re.sub("([0|1])\t", "", i)
             text_raw = re.sub("\n", "", text_raw)
             x.append(text_raw)
         return({"x":x, "y":y})
      
-twitter_2 = "C:\\Users\\donkey\\Desktop\\machine learning\\Data\\Sentiment\\text_emotion.csv"
-def twitter_sentiment_2(twitter_2_path):
-    df = pd.read_csv(twitter_2_path)
-    x = []; y = []
-    for i in range(len(df)):
-        if (df.ix[i,"sentiment"] == "happiness") or (df.ix[i,"sentiment"] == "fun") or (df.ix[i,"sentiment"] == "love") or (df.ix[i,"sentiment"] == "relief"):
-            y.append([1,0,0])
-        elif df.ix[i,"sentiment"] == "neutral":
-            y.append([0,1,0])
-        else:
-            y.append([0,0,1])                
-    x = list(df.ix[:,"content"])        
-    return({"x":x, "y":y})
 
 '''
 combined data in dictionary format
 data will be a dictionary of keys x and y. x: raw text and y: one-hot label     
 '''
+sentiment = {"x":review_sentiment(reviews)["x"] + twitter_sentiment(twitter)["x"], \
+             "y":review_sentiment(reviews)["y"] + twitter_sentiment(twitter)["y"]} 
 
-sentiment = {"x":review_sentiment(reviews)["x"] + twitter_sentiment(twitter)["x"] + twitter_sentiment_2(twitter_2)["x"], \
-             "y":review_sentiment(reviews)["y"] + twitter_sentiment(twitter)["y"] + twitter_sentiment_2(twitter_2)["y"]} 
 
-'''
-with ("sentiment_data.pickle", "wb") as save_file:
+with open("sentiment_data.pickle", "wb") as save_file:
     pickle.dump(sentiment, save_file)
     save_file.close()
-'''
+
     
 def word_frame(data = sentiment, file_name = "wordframe.pickle"):
     '''
@@ -81,12 +67,16 @@ def word_frame(data = sentiment, file_name = "wordframe.pickle"):
     each input data will be transformed to a count vector based on this template
     '''
     counter = []; text = data["x"] 
-    for i in text:                                         
+    status_check = 0
+    for i in text:
+        if status_check%1000 == 0:
+            print("covered rows of text: ", status_check)                                         
         text_token = nltk.tokenize.word_tokenize(i.lower())
         counter = counter + text_token #all words      
         stop = nltk.corpus.stopwords.words('english') + [",", ".", "-", "!", "?", ")", "(", ":", "'", ":", ";", "%", "&"]
         counter = [word for word in counter if not word in stop] #filter stop words from all words
-    clean = [nltk.stem.WordNetLemmatizer().lemmatize(word_wo_stop) for word_wo_stop in counter]    
+        status_check = status_check + 1
+    clean = [nltk.stem.WordNetLemmatizer().lemmatize(word_wo_stop, "v") for word_wo_stop in counter]    
     word_dictionary = collections.Counter(clean) #create count list from all words
     with open(file_name, "wb") as save_file:
         pickle.dump(word_dictionary, save_file)
@@ -94,8 +84,7 @@ def word_frame(data = sentiment, file_name = "wordframe.pickle"):
         print("word frame dictionary saved as pickle file") 
     return(word_dictionary) 
 
-
-    
+#word_frame()
     
 
 
